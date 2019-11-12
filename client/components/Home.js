73b5d72bcc2418b88255f4511fd2682fa47c8b9a
Theo Manton <theo333@gmail.com>
2019-11-12 18:49:17 -0500

    fix lint issues

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     this.state = {
       search: '',
       gifs: [],
-    }
+    };
   }
 
   // on mount, focus on search input
@@ -26,16 +26,23 @@ class Home extends Component {
     ev.preventDefault();
     console.log('handleSubmit search: ', this.state.search);
     axios
-      .get(`https://api.giphy.com/v1/gifs/search?api_key=w60ovrQO7lwrcHZrL9yjXigBdRHXV9cY&q=${this.state.search}&limit=3`)
-      .then(({ data: { data } }) => {
-        console.log('results: ', data);
-        console.log('gifs before: ', this.state.gifs)
-        this.setState(
-          { gifs: data })
-      }, () => console.log('gif after: ', this.state.gifs))
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=w60ovrQO7lwrcHZrL9yjXigBdRHXV9cY&q=${
+          this.state.search
+        }&limit=3`,
+      )
+      .then(
+        ({ data: { data } }) => {
+          console.log('results: ', data);
+          console.log('gifs before: ', this.state.gifs);
+          this.setState({ gifs: data });
+        },
+        () => console.log('gif after: ', this.state.gifs),
+      )
       .then(() => console.log('gifs after2: ', this.state.gifs))
       .catch(error => console.log(error));
   };
+
   render() {
     const { handleChange, handleSubmit } = this;
     const { search } = this.state;
@@ -43,23 +50,12 @@ class Home extends Component {
       <form onSubmit={handleSubmit} className="form-group">
         <label htmlFor="search" className="">
           Search
-            </label>
-        <input
-          id="search"
-          type="text"
-          name="search"
-          value={search}
-          onChange={handleChange}
-        />
-        <button
-          type='submit'
-        >
-          Search
-            </button>
+        </label>
+        <input id="search" type="text" name="search" value={search} onChange={handleChange} />
+        <button type="submit">Search</button>
       </form>
     );
-
-  };
+  }
 }
 
 export default Home;
