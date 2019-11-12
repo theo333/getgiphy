94b627c5276f44b1c3498c23c0cb2eb221a9fc9f
Theo Manton <theo333@gmail.com>
2019-11-12 18:48:01 -0500

    DOM render App, update index.html

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
+// ASK better way to do this?
+app.get('/app.js', (req, res, next) => res.sendFile(path.join(__dirname, '../public', 'main.js')));
+
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`app listening on port ${PORT}`);
 });
