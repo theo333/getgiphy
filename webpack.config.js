5a1d120d6fa923d24f6c3586798422788bd8cafd
Theo Manton <theo333@gmail.com>
2019-11-12 13:12:30 -0500

    add mode to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-	entry: './client/index.js',
-	output: {
-		path: __dirname,
-		filename: './public/main.js',
-	},
-	devtool: 'eval',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/main.js',
+  },
+  mode: 'development',
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
