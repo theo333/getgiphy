de0e7e7ef8d4570c2cee683a5f361751e1fad796
Theo Manton <theo333@gmail.com>
2019-11-12 18:43:29 -0500

    able to make api call and get results

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,32 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  const [search, setSearch] = useState('');
+// import SearchForm from './SearchForm';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      gifs: [],
+    }
+  }
 
   // on mount, focus on search input
-  useEffect(() => {
-    document.getElementById('search').focus();
-  }, []);
+  // useEffect(() => {
+  //   document.getElementById('search').focus();
+  // }, []);
 
-  const handleSubmit = ev => {
+  handleChange = ev => {
     ev.preventDefault();
-    console.log(search);
+    this.setState({ search: ev.target.value });
   };
 
-  return (
-    <form onSubmit={handleSubmit} className="form-group">
-      <label htmlFor="search" className="">
-        Search
-      </label>
-      <input
-        id="search"
-        type="text"
-        name="search"
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-      />
-    </form>
-  );
-};
+  handleSubmit = ev => {
+    ev.preventDefault();
+    console.log('handleSubmit search: ', this.state.search);
+    axios
+      .get(`https://api.giphy.com/v1/gifs/search?api_key=w60ovrQO7lwrcHZrL9yjXigBdRHXV9cY&q=${this.state.search}&limit=3`)
+      .then(({ data: { data } }) => {
+        console.log('results: ', data);
+        console.log('gifs before: ', this.state.gifs)
+        this.setState(
+          { gifs: data })
+      }, () => console.log('gif after: ', this.state.gifs))
+      .then(() => console.log('gifs after2: ', this.state.gifs))
+      .catch(error => console.log(error));
+  };
+  render() {
+    const { handleChange, handleSubmit } = this;
+    const { search } = this.state;
+    return (
+      <form onSubmit={handleSubmit} className="form-group">
+        <label htmlFor="search" className="">
+          Search
+            </label>
+        <input
+          id="search"
+          type="text"
+          name="search"
+          value={search}
+          onChange={handleChange}
+        />
+        <button
+          type='submit'
+        >
+          Search
+            </button>
+      </form>
+    );
+
+  };
+}
 
 export default Home;
