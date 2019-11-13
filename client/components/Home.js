59a4184b9c2825fdb7bfc7c6050a0547ec464e60
Theo Manton <theo333@gmail.com>
2019-11-12 19:46:33 -0500

    display gifs in list

--- client/components/Home.js
+++ client/components/Home.js
@@ -12,48 +12,69 @@ class Home extends Component {
     };
   }
 
-  // on mount, focus on search input
-  // useEffect(() => {
-  //   document.getElementById('search').focus();
-  // }, []);
+  componentDidMount() {
+    document.getElementById('search').focus();
+  }
 
   handleChange = ev => {
     ev.preventDefault();
     this.setState({ search: ev.target.value });
   };
 
-  handleSubmit = ev => {
-    ev.preventDefault();
-    console.log('handleSubmit search: ', this.state.search);
+  getGifs = () => {
+    const { search } = this.state;
+    console.log('handleSubmit search: ', search);
     axios
       .get(
-        `https://api.giphy.com/v1/gifs/search?api_key=w60ovrQO7lwrcHZrL9yjXigBdRHXV9cY&q=${
-          this.state.search
-        }&limit=3`,
+        `https://api.giphy.com/v1/gifs/search?api_key=w60ovrQO7lwrcHZrL9yjXigBdRHXV9cY&q=${search}&limit=3`,
       )
-      .then(
-        ({ data: { data } }) => {
-          console.log('results: ', data);
-          console.log('gifs before: ', this.state.gifs);
-          this.setState({ gifs: data });
-        },
-        () => console.log('gif after: ', this.state.gifs),
-      )
-      .then(() => console.log('gifs after2: ', this.state.gifs))
+      .then(({ data: { data } }) => {
+        console.log('results: ', data);
+        console.log('gifs before: ', this.state.gifs);
+        this.setState({ gifs: data });
+      })
+      .then(() => console.log('gifs after: ', this.state.gifs))
       .catch(error => console.log(error));
+  };
+
+  handleSubmit = ev => {
+    const { search } = this.state;
+    ev.preventDefault();
+    this.getGifs();
   };
 
   render() {
     const { handleChange, handleSubmit } = this;
-    const { search } = this.state;
+    const { search, gifs } = this.state;
     return (
-      <form onSubmit={handleSubmit} className="form-group">
-        <label htmlFor="search" className="">
-          Search
-        </label>
-        <input id="search" type="text" name="search" value={search} onChange={handleChange} />
-        <button type="submit">Search</button>
-      </form>
+      <Fragment>
+        <form onSubmit={handleSubmit} className="form-group">
+          <label htmlFor="search" className="">
+            Search
+          </label>
+          <input id="search" type="text" name="search" value={search} onChange={handleChange} />
+          <button type="submit">Search</button>
+        </form>
+        {gifs.length ? (
+          <ul>
+            {gifs.map(gif => {
+              const { title, images } = gif;
+              // eslint-disable-next-line camelcase
+              const { fixed_height } = images;
+              // eslint-disable-next-line camelcase
+              const { url, height, width } = fixed_height;
+              return (
+                <li key={gif.id}>
+                  {title}
+                  <img src={url} alt={title} height={height} width={width} />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          ''
+        )}
+      </Fragment>
     );
   }
 }
