11909409c63af0ca70d2443a2de48b6f2f121ff9
Theo Manton <theo333@gmail.com>
2019-11-14 16:33:44 -0500

    add favorites context to Favorites

--- client/components/Favorites.js
+++ client/components/Favorites.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import FavoritesContext from '../contexts/FavoritesContext';
 
 const Favorites = () => {
-  return <h1>Your Favorite GIFs</h1>;
+  return (
+    <FavoritesContext.Consumer>
+      {({ favorites, removeFavorite, toggleFavorite }) =>
+        favorites.length ? (
+          <ul id="gifs-main" className="container">
+            {console.log('favorites page: ', favorites)}
+            {favorites.map(gif => {
+              const { title, images } = gif;
+              // eslint-disable-next-line camelcase
+              const { fixed_height } = images;
+              // eslint-disable-next-line camelcase
+              const { url, height, width } = fixed_height;
+              return (
+                <li key={gif.id}>
+                  {/* TODO show on bottom part of image (CSS Grid), use caption for accessibility?
+                      {title} */}
+                  <div>
+                    <button name="fav" type="button" onClick={() => removeFavorite(gif)}>
+                      -
+                    </button>
+                    <button name="fav" type="button" onClick={() => toggleFavorite(gif)}>
+                      Toggle
+                    </button>
+                  </div>
+                  <img src={url} alt={title} height={height} width={width} />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          ''
+        )
+      }
+    </FavoritesContext.Consumer>
+  );
 };
 
 export default Favorites;
