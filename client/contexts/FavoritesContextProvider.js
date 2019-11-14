2a9b373c96f577593ca1d0df74286ff85f65a375
Theo Manton <theo333@gmail.com>
2019-11-14 16:32:08 -0500

    trying to fix isFavorite, but not working

--- client/contexts/FavoritesContextProvider.js
+++ client/contexts/FavoritesContextProvider.js
@@ -3,9 +3,20 @@ import React, { useState } from 'react';
 import FavoritesContext from './FavoritesContext';
 
 const FavoritesContextProvider = props => {
+  const isFavorite = (state, fav) => {
+    console.log('state in isFavorite(): ', state);
+    // const allFaves = state.favorites.map(x => x.isFav);
+    // console.log('allFaves: ', allFaves)
+    // const isFave = state.favorites.find(x => x.isFav === true);
+    const isFave = state.favorites.find(x => x.id === fav.id);
+    console.log('isFave; ', !!isFave, isFave);
+    return isFave;
+  };
+
   const addFavorite = fav => {
+    fav.isFav = true;
     setFavorites(prevState => {
-      const newFavorites = prevState.favorites.find(x => x.id === fav.id)
+      const newFavorites = isFavorite(prevState, fav)
         ? prevState.favorites
         : [...prevState.favorites, fav];
 
@@ -26,10 +37,26 @@ const FavoritesContextProvider = props => {
     });
   };
 
+  // const toggleFavorite = fav => !isFavorite(favorites, fav);
+
+  // TODO not working - isFavorite is not working ???
+  const toggleFavorite = fav => {
+    console.log('isFavorite: ', isFavorite(favorites, fav), favorites);
+    // const _isFavorite = isFavorite(favorites, fav)
+    //   ? 'remove now' //removeFavorite(fav)
+    //   : addFavorite(fav);
+    // console.log('_isFavorite: ', _isFavorite)
+    // return _isFavorite;
+    return isFavorite(favorites, fav)
+      ? 'remove now' //removeFavorite(fav)
+      : addFavorite(fav);
+  };
+
   const initialState = {
     favorites: [],
     addFavorite,
     removeFavorite,
+    toggleFavorite,
   };
 
   const [favorites, setFavorites] = useState(initialState);
