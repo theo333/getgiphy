212429d0fc56e409ac97fea14664ecec669a7610
Theo Manton <theo333@gmail.com>
2019-11-14 21:46:02 -0500

    add h1 to Favorites

--- client/components/Favorites.js
+++ client/components/Favorites.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import FavoritesContext from '../contexts/FavoritesContext';
 
 const Favorites = () => {
   return (
-    <FavoritesContext.Consumer>
-      {({ favorites, removeFavorite, toggleFavorite }) =>
-        favorites.length ? (
-          <ul id="gifs-main" className="container">
-            {console.log('favorites page: ', favorites)}
-            {favorites.map(gif => {
-              const { title, images } = gif;
-              // eslint-disable-next-line camelcase
-              const { fixed_height } = images;
-              // eslint-disable-next-line camelcase
-              const { url, height, width } = fixed_height;
-              return (
-                <li key={gif.id}>
-                  {/* TODO show on bottom part of image (CSS Grid), use caption for accessibility?
+    <Fragment>
+      <h1>My Giphy Favorites</h1>
+      <FavoritesContext.Consumer>
+        {({ favorites, removeFavorite, toggleFavorite }) =>
+          favorites.length ? (
+            <ul id="gifs-main" className="container">
+              {console.log('favorites page: ', favorites)}
+              {favorites.map(gif => {
+                const { title, images } = gif;
+                // eslint-disable-next-line camelcase
+                const { fixed_height } = images;
+                // eslint-disable-next-line camelcase
+                const { url, height, width } = fixed_height;
+                return (
+                  <li key={gif.id}>
+                    {/* TODO show on bottom part of image (CSS Grid), use caption for accessibility?
                       {title} */}
-                  <div>
-                    <button name="fav" type="button" onClick={() => removeFavorite(gif)}>
-                      -
-                    </button>
-                    <button name="fav" type="button" onClick={() => toggleFavorite(gif)}>
-                      Toggle
-                    </button>
-                  </div>
-                  <img src={url} alt={title} height={height} width={width} />
-                </li>
-              );
-            })}
-          </ul>
-        ) : (
-          ''
-        )
-      }
-    </FavoritesContext.Consumer>
+                    <div>
+                      <button name="fav" type="button" onClick={() => removeFavorite(gif)}>
+                        -
+                      </button>
+                      <button name="fav" type="button" onClick={() => toggleFavorite(gif)}>
+                        Toggle
+                      </button>
+                    </div>
+                    <img src={url} alt={title} height={height} width={width} />
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            ''
+          )
+        }
+      </FavoritesContext.Consumer>
+    </Fragment>
   );
 };
 
