701853da0e6dfac0bf26598c3c1ecb8cdacc574d
Theo Manton <theo333@gmail.com>
2019-11-15 12:33:00 -0500

    refactor Search from class to functional component, useState for state

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,5 +1,5 @@
 /*eslint-disable consistent-return*/
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import axios from 'axios';
 import MdHeart from 'react-ionicons/lib/MdHeart';
 
@@ -7,16 +7,10 @@ import { apiKey } from '../../.env';
 import FavoritesContext from '../contexts/FavoritesContext';
 import SearchForm from './SearchForm';
 
-class Search extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentSearch: '', // eslint-disable-line react/no-unused-state
-      gifs: [],
-    };
-  }
+const Search = () => {
+  const [gifs, setGifs] = useState([]);
 
-  getGifs = async search => {
+  const getGifs = async search => {
     try {
       const response = await axios.get(
         `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=8`,
@@ -28,65 +22,62 @@ class Search extends Component {
     }
   };
 
-  handleSubmit = async query => {
+  const handleSubmit = async query => {
     try {
-      const gifs = await this.getGifs(query);
-      this.setState({ gifs });
+      const newGifs = await getGifs(query);
+      setGifs(newGifs);
     } catch (error) {
       console.log('Search handleSubmit error: ', error);
     }
   };
 
-  render() {
-    const { gifs } = this.state;
-    return (
-      <Fragment>
-        <h1>Search for Your Favorite GIFs!</h1>
-        <SearchForm onSubmit={this.handleSubmit} />
-        {gifs.length ? (
-          <Fragment>
-            <ul id="gifs-main" className="container">
-              {gifs.map(gif => {
-                // eslint-disable-next-line camelcase
-                const {
-                  title,
-                  images: {
-                    fixed_height: { url, height, width },
-                  },
-                } = gif;
-                return (
-                  <li key={gif.id}>
-                    {/* TODO show on bottom part of image (CSS Grid), use caption for accessibility?
+  return (
+    <Fragment>
+      <h1>Search for Your Favorite GIFs!</h1>
+      <SearchForm onSubmit={handleSubmit} />
+      {gifs.length ? (
+        <Fragment>
+          <ul id="gifs-main" className="container">
+            {gifs.map(gif => {
+              // eslint-disable-next-line camelcase
+              const {
+                title,
+                images: {
+                  fixed_height: { url, height, width },
+                },
+              } = gif;
+              return (
+                <li key={gif.id}>
+                  {/* TODO show on bottom part of image (CSS Grid), use caption for accessibility?
                     {title} */}
-                    <div className="card">
-                      <FavoritesContext.Consumer>
-                        {({ favorites, toggleFavorite, isFavorite }) => (
-                          <Fragment>
-                            {console.log('favorites: ', favorites)}
-                            <button
-                              name="favorite-toggle"
-                              type="button"
-                              className="btn-heart"
-                              onClick={() => toggleFavorite(gif)}
-                            >
-                              <MdHeart fontSize="25px" color={isFavorite(gif) ? 'red' : 'black'} />
-                            </button>
-                          </Fragment>
-                        )}
-                      </FavoritesContext.Consumer>
-                      <img src={url} alt={title} height={height} width={width} />
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </Fragment>
-        ) : (
-          ''
-        )}
-      </Fragment>
-    );
-  }
-}
+                  <div className="card">
+                    <FavoritesContext.Consumer>
+                      {({ favorites, toggleFavorite, isFavorite }) => (
+                        <Fragment>
+                          {console.log('favorites: ', favorites)}
+                          <button
+                            name="favorite-toggle"
+                            type="button"
+                            className="btn-heart"
+                            onClick={() => toggleFavorite(gif)}
+                          >
+                            <MdHeart fontSize="25px" color={isFavorite(gif) ? 'red' : 'black'} />
+                          </button>
+                        </Fragment>
+                      )}
+                    </FavoritesContext.Consumer>
+                    <img src={url} alt={title} height={height} width={width} />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </Fragment>
+      ) : (
+        ''
+      )}
+    </Fragment>
+  );
+};
 
 export default Search;
