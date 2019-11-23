8592bb86b28e5f923db43a68a45ae5fc40814381
Theo Manton <theo333@gmail.com>
2019-11-22 21:01:27 -0500

    fix html and padding

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
   const year = new Date().getFullYear();
 
-  return <footer className="Footer">&copy; {year} GetGiphy</footer>;
+  return (
+    <footer className="Footer">
+      <p>&copy; {year} GetGiphy</p>
+    </footer>
+  );
 };
 
 export default Footer;
